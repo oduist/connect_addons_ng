eaf67c445e8077b64de7bb860e5e0ea7d52c8136
Max <litnimax@users.noreply.github.com>
2026-05-25 18:52:22 +0300

    [IMP] Verto login: <login-local><res.users.id> for readability (#75)
    Co-authored-by: David Brühlmeier <31269608+dbruehlmeier@users.noreply.github.com>
    Co-authored-by: Sozialinfo DevOps <devops@sozialinfo.ch>

--- connect_freeswitch/__manifest__.py
+++ connect_freeswitch/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Oduist Connect FreeSWITCH',
-    'version': '18.0.1.8.18',
+    'version': '18.0.1.8.19',
     'author': 'Oduist',
     'category': 'Phone',
     'summary': 'FreeSWITCH integration for Oduist Connect',
