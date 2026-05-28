6dc3eed2efe2040f4e807bb48d8348c410754ace
litnimax <litnimaxster@gmail.com>
2026-05-28 16:33:54 +0300

    chore(freeswitch): bump version to 19.0.1.9.6

--- connect_freeswitch/__manifest__.py
+++ connect_freeswitch/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Oduist Connect FreeSWITCH',
-    'version': '19.0.1.9.5',
+    'version': '19.0.1.9.6',
     'author': 'Oduist',
     'category': 'Phone',
     'summary': 'FreeSWITCH integration for Oduist Connect',
