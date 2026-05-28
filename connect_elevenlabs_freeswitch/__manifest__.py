73406313d00a06606d7c253cfea184d0a7705527
Nicolae Postica <nicolae.postica2@gmail.com>
2026-05-29 02:29:27 +0300

    [connect_elevenlabs_freeswitch] bump for agent provider_id (ODU-24)

--- connect_elevenlabs_freeswitch/__manifest__.py
+++ connect_elevenlabs_freeswitch/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'Connect ElevenLabs - FreeSWITCH Bridge',
-    'version': '1.1.4',
+    'version': '1.1.5',
     'author': 'Oduist',
     'maintainer': 'Oduist',
     'support': 'support@oduist.com',
