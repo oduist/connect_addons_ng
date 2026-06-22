4e77a4ab23b638977f15849c7698dbcf11fdb309
Nicolae Postica <nicolae.postica2@gmail.com>
2026-06-22 17:59:15 +0300

    [connect] move provider tags after Odoo User on user form (ODU-40)

--- connect/__manifest__.py
+++ connect/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Oduist Connect',
-    'version': '19.0.3.1.18',
+    'version': '19.0.3.1.19',
     'author': 'Oduist',
     'category': 'Phone',
     'summary': 'Communication platform for Odoo',
