d7ebff6cb5f694b09cdaddc1c2ecd22aa0601c55
Nicolae Postica <nicolae.postica2@gmail.com>
2026-06-22 18:01:23 +0300

    [connect_twilio] anchor username after provider tags so Providers sits right below Odoo User (ODU-40)

--- connect_twilio/__manifest__.py
+++ connect_twilio/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Oduist Connect Twilio',
-    'version': '19.0.1.1.16',
+    'version': '19.0.1.1.17',
     'author': 'Oduist',
     'category': 'Phone',
     'summary': 'Twilio integration for Oduist Connect',
