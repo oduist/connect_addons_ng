52a3f3f3ca27c7252d00f9c713ce118d87e88dad
Nicolae Postica <nicolae.postica2@gmail.com>
2026-06-22 17:08:56 +0300

    [connect] de-dup Provider field on connect.user form (ODU-39)

--- connect/__manifest__.py
+++ connect/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Oduist Connect',
-    'version': '19.0.3.1.16',
+    'version': '19.0.3.1.17',
     'author': 'Oduist',
     'category': 'Phone',
     'summary': 'Communication platform for Odoo',
