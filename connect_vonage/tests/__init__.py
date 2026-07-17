f3d6af6d1ee2549c2ac7686f0a43418e1b62b5ee
Max <litnimax@users.noreply.github.com>
2026-07-17 19:15:26 +0300

    feat(vonage): add Vonage provider integration (#180)

---
+++ connect_vonage/tests/__init__.py
@@ -0,0 +1,7 @@
+# Tests for connect_vonage module.
+from . import test_acl
+from . import test_message
+from . import test_ncco_render
+from . import test_recording
+from . import test_settings
+from . import test_voice_events
